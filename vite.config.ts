ecb30c3a5bcdaf4c3905e0e365cb3f9af3c81b34
SurajEdit <ajha6643@gmail.com>
2026-02-03 15:32:46 +0530

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-
+base: '/webprimeaixyz/'
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
